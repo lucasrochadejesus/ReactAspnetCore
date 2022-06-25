201fedb1a82a54e73eb0e949d5cea6d7ce68affe
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2022-06-24 22:27:07 -0500

    Add JsonSerializer
    Added JsonSerializer config.

--- back/proactivity.API/Program.cs
+++ back/proactivity.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using proactivity.API.Repository;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,12 @@ builder.Services.AddDbContext<DataContext>(
     options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
 );
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(option =>
+        {
+            option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        }
+    );
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
